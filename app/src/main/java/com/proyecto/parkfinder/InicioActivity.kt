8f8d1cf3bcb1e2e1d1e7da71a8d7736589919d8e
Juan Pablo Vargas <38387499+PabloVargasG@users.noreply.github.com>
2023-06-21 21:07:11 -0600

    pantalla principal 1.0 prevista menu lateral y busqueda

--- app/src/main/java/com/proyecto/parkfinder/InicioActivity.kt
+++ app/src/main/java/com/proyecto/parkfinder/InicioActivity.kt
@@ -4,14 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 
 class InicioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
+        val btnMuseos = findViewById<ImageButton>(R.id.imageButtonMuseos)
 
-        val redireccionar = findViewById<TextView>(R.id.textView2)
+        val btnTeatros = findViewById<ImageButton>(R.id.imageButtonTeatros)
+        val btnParques = findViewById<ImageButton>(R.id.imageButtonParques)
+        val btnMenuLat = findViewById<ImageButton>(R.id.imageButtonMenu)
+
+
+
+        val redireccionar = findViewById<TextView>(R.id.BannerTxtView)
         redireccionar.setOnClickListener{
             val intent = Intent(this, AtraccionesPorCategoriaActivity::class.java)
             startActivity(intent)
